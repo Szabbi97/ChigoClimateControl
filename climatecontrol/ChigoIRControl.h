89c655f52b5f457e412e9aebdb008641451da009
Szabolcs Berényi <szabolcs.berenyi@semilab.hu>
2025-08-01 18:14:15 +0200

    feat: ir control extract

---
+++ climatecontrol/ChigoIRControl.h
@@ -0,0 +1,34 @@
+#ifndef CHIGO_IR_CONTROL_H
+#define CHIGO_IR_CONTROL_H
+
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+class ChigoIRControl {
+public:
+    ChigoIRControl(uint16_t irLedPin);
+    void begin();
+    void powerOn();
+    void powerOff();
+    bool setTemperature(int temp);
+
+private:
+    IRsend irsend;
+    static const uint16_t rawData_on[99];
+    static const uint16_t rawData_off[99];
+    static const uint16_t rawData_18[99];
+    static const uint16_t rawData_19[99];
+    static const uint16_t rawData_20[99];
+    static const uint16_t rawData_21[99];
+    static const uint16_t rawData_22[99];
+    static const uint16_t rawData_23[99];
+    static const uint16_t rawData_24[99];
+    static const uint16_t rawData_25[99];
+    static const uint16_t rawData_26[99];
+    static const uint16_t rawData_27[99];
+    static const uint16_t rawData_28[99];
+    static const uint16_t rawData_29[99];
+    static const uint16_t rawData_30[99];
+};
+
+#endif // CHIGO_IR_CONTROL_H
